7bb8c0b95dd64f83bfe167af05cdf33133747783
vithusa514 <vithusakonesvaran75@gmail.com>
2026-04-28 09:48:25 +0530

    Add Item Manager Project

---
+++ WMT_LabTest_Project/backend/models/Item.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Item name is required"],
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price cannot be negative"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    customerReviewCount: {
+      type: Number,
+      default: 0,
+      min: [0,"Review count cannot be negative"]
+    },
+
+
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Item", itemSchema);
