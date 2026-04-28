4220c12b6f8c52f1e246d6bde639e15260793efb
vithusa514 <vithusakonesvaran75@gmail.com>
2026-04-28 10:30:04 +0530

    Fix API URL for deployments

--- WMT_LabTest_Project/frontend/src/api/itemApi.js
+++ WMT_LabTest_Project/frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://itemmanagerproject-production-912f.up.railway.app/",
+  baseURL: import.meta.env.VITE_API_URL || "https://itemmanagerproject-production-912f.up.railway.app",
 });
 
 export const getItems = () => API.get("/items");
